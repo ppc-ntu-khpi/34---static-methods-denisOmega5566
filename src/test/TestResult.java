1d98a72f7d85ffe29881c80a1c07cac7d18cf874
denisOmega5566 <74858963+denisOmega5566@users.noreply.github.com>
2021-01-31 15:08:45 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,8 @@
 package test;
-
 import domain.Exercise;
-
 public class TestResult {
-
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        String s = "03.09.2003";
+        System.out.println("You number of fate = "+ Exercise.Calculate(s));
     }
 }
